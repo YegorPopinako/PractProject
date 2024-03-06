30663f1e7528c715bc62c2e01bc55b657c9a9a41
Yegor Popinako <yegor5x@gmail.com>
2024-03-06 16:19:57 +0200

    fixed unfolded шшьзÑimports

--- src/main/java/ua/petproject/model/Element.java
+++ src/main/java/ua/petproject/model/Element.java
@@ -3,7 +3,11 @@ package ua.petproject.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 import ua.petproject.model.categories.Category;
 
 @Getter

bfcc1696f1d56ae6f223e9d6f42349c931193acd
Yegor Popinako <yegor5x@gmail.com>
2024-03-08 15:12:46 +0200

    added annotation to Element class

--- src/main/java/ua/petproject/model/Element.java
+++ src/main/java/ua/petproject/model/Element.java
@@ -1,6 +1,8 @@
 package ua.petproject.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -26,7 +28,12 @@ public class Element {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false)
+    @Enumerated(value = jakarta.persistence.EnumType.STRING)
     private Category category;
 
     @Override

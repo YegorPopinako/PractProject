734c6e1f9d631cb300a293d83f1c8e6f2697fa50
Yegor Popinako <yegor5x@gmail.com>
2024-05-26 19:20:15 +0300

    fixed security configuration

--- src/main/java/ua/petproject/config/SecurityConfig.java
+++ src/main/java/ua/petproject/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -13,8 +14,8 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(authorize -> authorize
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeRequests(authorize -> authorize
                         .requestMatchers("/login", "/register", "/api/books", "/css/**", "/js/**")
                         .permitAll()
                 )

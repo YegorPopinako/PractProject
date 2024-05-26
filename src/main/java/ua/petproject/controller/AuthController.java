0c1acee014675d1b97cc612560dc13fec4828eca
Yegor Popinako <yegor5x@gmail.com>
2024-05-26 18:14:54 +0300

    cnofigured security

--- src/main/java/ua/petproject/controller/AuthController.java
+++ src/main/java/ua/petproject/controller/AuthController.java
@@ -17,6 +17,11 @@ public class AuthController {
 
     private UserService userService;
 
+    @GetMapping("/login")
+    public String getLoginForm() {
+        return "login";
+    }
+
     @GetMapping("/register")
     public String getRegisterForm(Model model) {
         User user = new User();
@@ -28,11 +33,11 @@ public class AuthController {
     public String register(@ModelAttribute("user") @Valid User user, BindingResult result, Model model) {
         User existingUserEmail = userService.findByEmail(user.getEmail());
         if(existingUserEmail != null && existingUserEmail.getEmail() != null && !existingUserEmail.getEmail().isEmpty()) {
-            result.rejectValue("email", "There is already user registered with the email/username provided");
+            return "redirect:/register?fail";
         }
         User existingUserUsername = userService.findByUsername(user.getUsername());
         if(existingUserUsername != null && existingUserUsername.getUsername() != null && !existingUserUsername.getUsername().isEmpty()) {
-            result.rejectValue("username", "There is already user registered with the email/username provided");
+            return "redirect:/register?fail";
         }
         if (result.hasErrors()) {
             model.addAttribute("user", user);
